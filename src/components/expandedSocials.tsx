4684618d62d0a06e702b9cb81020d9257ffff2c5
Jack Sanders <jacksanders404@gmail.com>
2023-05-23 23:21:13 +0100

    Made Socials null-safe

---
+++ src/components/expandedSocials.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function ExpandedSocials(): React.ReactElement {
+  return (
+    <div className="flex items-center gap-6 text-3xl">
+      <a
+        className="bi bi-instagram hover-link dark:hover-link-dark"
+        href="https://www.instagram.com/jacksanders02/"
+      />
+      <a
+        className="bi bi-linkedin hover-link dark:hover-link-dark"
+        href="https://www.linkedin.com/in/jack-sanders-05915423a/"
+      />
+      <a
+        className="bi bi-github hover-link dark:hover-link-dark"
+        href="https://github.com/JSanders02"
+      />
+      <div
+        className={`self-stretch border-r-2 border-on-background 
+                      dark:border-on-background-dark`}
+      />
+      <a className={`hover-link dark:hover-link-dark text-lg underline`}>
+        more about me
+      </a>
+    </div>
+  );
+}
--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -1,12 +1,13 @@
 "use client";
 import React, { useEffect } from "react";
 import MediaQuery from "react-responsive";
+import ExpandedSocials from "@/components/expandedSocials";
 
 export default function Socials(): React.ReactElement {
   const [mounted, setMounted] = React.useState(false);
   useEffect(() => {
     setMounted(true);
-  });
+  }, []);
 
   if (mounted) {
     return (
@@ -17,29 +18,11 @@ export default function Socials(): React.ReactElement {
           </div>
         </MediaQuery>
         <MediaQuery minWidth={1200}>
-          <div className="flex items-center gap-6 text-3xl">
-            <a
-              className="bi bi-instagram hover-link dark:hover-link-dark"
-              href="https://www.instagram.com/jacksanders02/"
-            />
-            <a
-              className="bi bi-linkedin hover-link dark:hover-link-dark"
-              href="https://www.linkedin.com/in/jack-sanders-05915423a/"
-            />
-            <a
-              className="bi bi-github hover-link dark:hover-link-dark"
-              href="https://github.com/JSanders02"
-            />
-            <div
-              className={`self-stretch border-r-2 border-on-background 
-                      dark:border-on-background-dark`}
-            />
-            <a className={`hover-link dark:hover-link-dark text-lg underline`}>
-              more about me
-            </a>
-          </div>
+          <ExpandedSocials />
         </MediaQuery>
       </div>
     );
+  } else {
+    return (<ExpandedSocials />)
   }
 }
