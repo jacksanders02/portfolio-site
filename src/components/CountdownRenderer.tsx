3b866ab66e2be32ca8da27577e559026c3ac6d08
Jack Sanders <jacksanders404@gmail.com>
2024-01-14 19:09:56 +0100

    📦️ Updated packages per dependabot recommendations

--- src/components/CountdownRenderer.tsx
+++ src/components/CountdownRenderer.tsx
@@ -2,21 +2,19 @@ import React from "react";
 import { CountdownRenderProps } from "react-countdown";
 
 /**
- * A responsive parallax for the hero section.
- * On smaller heights, the list of projects sometimes catches up to the website title, so the best solution is to start
- * with the parallax being faster, and then slow it down as the screen size increases
- * @param children {React.JSX.Element[]} The elements that should go inside the Parallax
+ * A custom renderer for react-countdown (Changes format from DD:HH:MM:SS to DD-HH:MM:SS, which is more readable)
  * @constructor
+ * @param props
  */
 export default function CountdownRenderer(props: CountdownRenderProps): React.ReactElement {
   return (
     <div className={`flex flex-row items-center ${props.props.className}`}>
       <span>{props.formatted.days}</span>
-      <span className={`mb-2 md:mb-3 lg:mb-5`}>-</span>
+      -
       <span>{props.formatted.hours}</span>
-      <span className={`mb-2 md:mb-3 lg:mb-5`}>:</span>
+      <span className={`mb-1 md:mb-3 lg:mb-4`}>:</span>
       <span>{props.formatted.minutes}</span>
-      <span className={`mb-2 md:mb-3 lg:mb-5`}>:</span>
+      <span className={`mb-1 md:mb-3 lg:mb-4`}>:</span>
       <span>{props.formatted.seconds}</span>
     </div>
   );
