7eeb441c156a706d2303e38abed46ddb56df424d
Jack Sanders <jacksanders404@gmail.com>
2024-03-03 22:33:48 +0000

    🍱 Added 'Not by AI' to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 import { fontSerif } from '@/app/fonts';
 
 /**
@@ -8,41 +10,62 @@ export default function Footer(): React.ReactNode {
   return (
     <footer className="flex justify-center items-center">
       <div
-        className="p-4 border-t-2 border-on-background dark:border-on-background-dark w-max flex flex-col items-center text-center"
+        className="p-4 border-t-2 border-on-background dark:border-on-background-dark w-max flex flex-col items-center text-center gap-4"
       >
         <p
-          className={`${fontSerif.className} text-3xl sm:text-4xl lg:text-5xl text-center italic mb-4`}
+          className={`${fontSerif.className} text-3xl sm:text-4xl lg:text-5xl text-center italic`}
         >
           JS
         </p>
         <p>
           Built by myself using
           {' '}
-          <a
-            href="nextjs.com"
+          <Link
+            href="https://nextjs.org"
+            target="_blank"
             className="hover-link dark:hover-link-dark underline"
           >
             Next.js
-          </a>
+          </Link>
           {' '}
           and
           {' '}
-          <a
-            href="tailwindcss.com"
+          <Link
+            href="https://tailwindcss.com"
+            target="_blank"
             className="hover-link dark:hover-link-dark underline"
           >
             Tailwind
-          </a>
+          </Link>
           . Deployed with
           {' '}
-          <a
-            href="vercel.com"
+          <Link
+            href="https://vercel.com"
+            target="_blank"
             className="hover-link dark:hover-link-dark underline"
           >
             Vercel
-          </a>
+          </Link>
           .
         </p>
+        <div className="flex flex-row gap-4">
+          <Link href="https://notbyai.fyi/" target="_blank">
+            <Image
+              src="/not-by-ai/by-human-white.svg"
+              alt="Badge indicating that this page was not written by AI"
+              width={150}
+              height={100}
+            />
+          </Link>
+          <Link href="https://notbyai.fyi/" target="_blank">
+            <Image
+              src="/not-by-ai/door-mensen-white.svg"
+              alt="Badge indicating that this page was not written by AI"
+              width={150}
+              height={100}
+            />
+          </Link>
+        </div>
       </div>
     </footer>
   );
